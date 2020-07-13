dbdad0baafe720ef1fcbb0ef46cec6c61c3d65f6
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-13 21:45:50 +0200

    fix controllo stringa vuota + overflow + ora messaggii

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 function sendMsg() {
   var myClone = $('.template .sent-message').clone();
   var myMessage = $("#message").val();
-  myClone.text(myMessage);
+  var dt = new Date();
+  var time = dt.getHours() + ":" + dt.getMinutes()
+  myClone.children("p").text(myMessage);
+  myClone.children("span").text(time);
   $('#chat-main').append(myClone);
 
 }
 
 function addAnswer() {
   var myClone = $('.template .answer').clone();
+  var dt = new Date();
+  var time = dt.getHours() + ":" + dt.getMinutes()
+  myClone.children("span").text(time);
   $('#chat-main').append(myClone);
 };
 
@@ -16,7 +22,7 @@ function init() {
   var sendBtn = $("#send-button");
   sendBtn.click(function() {
   // CONTROLLO STRINGA VUOTA
-  if ($("#message").val()) {
+  if ($("#message").val() != false) {
     sendMsg();
     // RISPOSTA DOPO 1 S
     setTimeout(addAnswer, 1000);
@@ -26,10 +32,11 @@ function init() {
     });
  // MANDA MESSAGGIO PRESS ENTER
   $('.input').keydown(function(event){
-  if (event.which == 13) {
+  if (event.which == 13 && $("#message").val() != false) {
     sendMsg();
     // RISPOSTA DOPO 1 S
     setTimeout(addAnswer, 1000);
+    $("#message").val(" ");
   }
   });
 }
