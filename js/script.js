98cb1a690ecdc2fc8825c50d9cbaa5e5fe3fcb0a
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-13 16:09:31 +0200

    JS funzione risposta ok

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 function sendMsg() {
-  var myClone = $('.template div').clone();
+  var myClone = $('.template .sent-message').clone();
   var myMessage = $("#message").val();
-  console.log(myMessage);
   myClone.text(myMessage);
   $('#chat-main').append(myClone);
 }
 
+function addAnswer() {
+  var myClone = $('.template .answer').clone();
+  $('#chat-main').append(myClone);
+};
+
 function init() {
   // MANDA MESSAGGIO CLICK BTN
   var sendBtn = $("#send-button");
-  sendBtn.click(sendMsg);
+  sendBtn.click(function() {
+  sendMsg();
+  setTimeout(addAnswer, 1000);
+    });
  // MANDA MESSAGGIO PRESS ENTER
   $('.input').keydown(function(event){
   if (event.which == 13) {
     sendMsg();
+    setTimeout(addAnswer, 1000);
   }
   });
 }
