854cd4acf9f378158a6797142da8b2058253de7b
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-13 12:04:09 +0200

    aggiunta invio msg con tasto enter

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-
+function sendMsg() {
+  var myClone = $('.template div').clone();
+  var myMessage = $("#message").val();
+  console.log(myMessage);
+  myClone.text(myMessage);
+  $('#chat-main').append(myClone);
+}
 
 function init() {
+  // MANDA MESSAGGIO CLICK BTN
   var sendBtn = $("#send-button");
-  sendBtn.click(function() {
-    var myClone = $('.template div').clone();
-    var myMessage = $("#message").val();
-    console.log(myMessage);
-    myClone.text(myMessage);
-    $('#chat-main').append(myClone);
-
+  sendBtn.click(sendMsg);
+ // MANDA MESSAGGIO PRESS ENTER
+  $('.input').keydown(function(event){
+  if (event.which == 13) {
+    sendMsg();
+  }
   });
-
 }
 
 
